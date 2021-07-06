826058720db5b63d77e9ed9afe46bd7bc003804d
diegomohammadbrazil <binorx@hotmail.com>
2021-07-06 17:12:13 -0300

    calculadora de danos v2 commit

---
+++ Projeto 02 - Calculadora de Dano v3/index.js
@@ -0,0 +1,93 @@
+const vidaPorPersonagem = {
+    'Seiya de Pegaso': 8,
+    'Shyriu de Dragão': 12,
+    'Hyoga de Cisne': 18,
+    'Shun de Andromeda': 12,
+    'Ikki de Fênix': 12,
+
+
+};
+
+const danoPorArma = {
+    'meteoro de pegasus': 2,
+    'colera do dragão': 5,
+    'pó de diamante': 10,
+    'corrente de andromeda': 10,
+    'colera do dragão': 5,
+    'ave fênix': 10,
+
+};
+
+let personagemSelecionado;
+let armaSelecionada;
+
+function iniciar() {
+    const elementos = document.getElementsByClassName('elemento');
+
+    for (const elemento of elementos) {
+        elemento.addEventListener('click', marcarElementoSelecionado);
+    }
+
+    document.getElementById('calcular').addEventListener('click', calcularDano);
+}
+
+function marcarElementoSelecionado(evento) {
+    const elementoSelecionado = evento.target.parentElement;
+
+    // Se o bloco clicado n�o possui a classe 'elemento', consideramos que n�o
+    // � um clique v�lido e encerramos a fun��o, retornando algo vazio.
+    if (!elementoSelecionado.classList.contains('elemento')) {
+        return;
+    }
+
+    const idElementoSelecionado = elementoSelecionado.getAttribute('id');
+
+    if (elementoSelecionado.classList.contains('personagem')) {
+        personagemSelecionado = idElementoSelecionado;
+        limparElementosSelecionados('personagem');
+    } else {
+        armaSelecionada = idElementoSelecionado;
+        limparElementosSelecionados('arma');
+    }
+
+    elementoSelecionado.classList.add('selecionado');
+}
+
+function calcularDano() {
+    if (!personagemSelecionado || !armaSelecionada) {
+        alert('Selecione o personagem e a arma para calcular o dano');
+        return;
+    }
+
+    const danoDados = rolarOsDados();
+    const danoArma = danoPorArma[armaSelecionada];
+    const danoTotal = danoDados + danoArma;
+    const vidaPersonagem = vidaPorPersonagem[personagemSelecionado];
+
+    let resultado = 'Dano: ' + danoTotal + '! ';
+
+    if (danoTotal >= vidaPersonagem) {
+        resultado += 'Parab�ns, voc� matou ' + personagemSelecionado;
+    } else {
+        resultado += 'Putz, n�o foi dessa vez, tente novamente!';
+    }
+
+    document.getElementById('dano').innerHTML = resultado;
+}
+
+function limparElementosSelecionados(tipo) {
+    const elementos = document.getElementsByClassName('elemento');
+
+    for (const elemento of elementos) {
+        if (elemento.classList.contains(tipo)) {
+            elemento.classList.remove('selecionado');
+        }
+    }
+}
+
+function rolarOsDados() {
+    const min = Math.ceil(1);
+    const max = Math.floor(10);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
